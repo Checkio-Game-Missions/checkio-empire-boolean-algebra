2a0d3197389fa31839beb2cbc4d873a068f414e4
Valentin Bryukhanov <bvv.mag@gmail.com>
2014-08-28 10:46:28 +1200

    tests

--- verification/tests.py
+++ verification/tests.py
@@ -7,30 +7,87 @@ Each test is dict with
     "explanation" -- not necessary key, it's using for additional info in animation.
 """
 
-
 TESTS = {
-    "Basics": [
+    "All": [
         {
-            "input": [3, 2],
-            "answer": 5,
-            "explanation": "3+2=?"
+            "input": [0, 0, 'conjunction'],
+            "answer": 0,
         },
         {
-            "input": [5, 7],
-            "answer": 12,
-            "explanation": "5+7=?"
-        }
-    ],
-    "Extra": [
-        {
-            "input": [6, 3],
-            "answer": 9,
-            "explanation": "6+3=?"
+            "input": [0, 1, 'conjunction'],
+            "answer": 0,
         },
         {
-            "input": [6, 7],
-            "answer": 13,
-            "explanation": "6+7=?"
-        }
+            "input": [1, 0, 'conjunction'],
+            "answer": 0,
+        },
+        {
+            "input": [1, 1, 'conjunction'],
+            "answer": 1,
+        },
+        {
+            "input": [0, 0, 'disjunction'],
+            "answer": 0,
+        },
+        {
+            "input": [0, 1, 'disjunction'],
+            "answer": 1,
+        },
+        {
+            "input": [1, 0, 'disjunction'],
+            "answer": 1,
+        },
+        {
+            "input": [1, 1, 'disjunction'],
+            "answer": 1,
+        },
+        {
+            "input": [0, 0, 'implication'],
+            "answer": True,
+        },
+        {
+            "input": [0, 1, 'implication'],
+            "answer": True,
+        },
+        {
+            "input": [1, 0, 'implication'],
+            "answer": 0,
+        },
+        {
+            "input": [1, 1, 'implication'],
+            "answer": 1,
+        },
+        {
+            "input": [0, 0, 'exclusive'],
+            "answer": 0,
+        },
+        {
+            "input": [0, 1, 'exclusive'],
+            "answer": 1,
+        },
+        {
+            "input": [1, 0, 'exclusive'],
+            "answer": 1,
+        },
+        {
+            "input": [1, 1, 'exclusive'],
+            "answer": 0,
+        },
+        {
+            "input": [0, 0, 'equivalence'],
+            "answer": True,
+        },
+        {
+            "input": [0, 1, 'equivalence'],
+            "answer": False,
+        },
+        {
+            "input": [1, 0, 'equivalence'],
+            "answer": False,
+        },
+        {
+            "input": [1, 1, 'equivalence'],
+            "answer": True,
+        },
     ]
 }
