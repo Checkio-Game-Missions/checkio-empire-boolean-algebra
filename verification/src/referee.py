68fab4426e5acda60cc5f851e52696e6f0306a5d
Valentin Bryukhanov <bvv.mag@gmail.com>
2015-03-16 17:18:37 +0300

    add representation

--- verification/src/referee.py
+++ verification/src/referee.py
@@ -1,4 +1,5 @@
 from checkio_referee import RefereeBase
+from checkio_referee import representations
 
 import settings
 import settings_env
@@ -15,6 +16,11 @@ class Referee(RefereeBase):
     EXECUTABLE_PATH = settings.EXECUTABLE_PATH
     CURRENT_ENV = settings_env.CURRENT_ENV
     FUNCTION_NAME = "boolean"
+    CALLED_REPRESENTATIONS = {
+        "python_3": representations.unwrap_arg_representation,
+        "python_2": representations.unwrap_arg_representation,
+        "javascript": representations.unwrap_arg_representation
+    }
     ENV_COVERCODE = {
         "python_2": cover,
         "python_3": cover,
